be2baf052f49adceeee181e7c03be23ec19525e1
Harrison Erio <u7541840@gmail.com>
2024-04-28 16:08:26 +0800

    initial

---
+++ leetcode/lee2923.cpp
@@ -0,0 +1,22 @@
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int findChampion(vector<vector<int>>& grid) {
+        int n = grid.size();
+        for(int i=0;i<n;++i){
+            bool isChampion = true;
+            for(int j = 0; j < n; ++j){
+                if(i!=j && grid[i][j] ==0){
+                    isChampion = false;
+                    break;
+                }
+            }
+            if(isChampion){
+                return i;
+            }
+        }
+        return -1;
+    }
+};
