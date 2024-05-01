9d15df317e336cdbc17fea616d79d7a4f0923400
harrison <u7541840@gmail.com>
2024-05-01 11:29:09 +1000

    update cmakelists

--- acwing/arrange_numbers.cpp
+++ acwing/arrange_numbers.cpp
@@ -6,36 +6,36 @@ vector<vector<int>> solutions;
 vector<int> solution;
 vector<int> state;
 
-void dfs(int current, int n){
-    if(current == n) {
+void dfs(int current, int n) {
+    if (current == n) {
         solutions.push_back(solution);
         return;
     }
-    for(int i=1; i<=n; ++i ){
-        if(!state[i]){
+    for (int i = 1; i <= n; ++i) {
+        if (!state[i]) {
             solution[current] = i;
             state[i] = 1;
-            dfs(current+1,n);
+            dfs(current + 1, n);
             state[i] = 0;
         }
     }
 }
-void solveNumbers(int n){
-    solution = vector<int>(n,0);
-    state = vector<int>(n,0);
+
+void solveNumbers(int n) {
+    solution = vector<int>(n, 0);
+    state = vector<int>(n, 0);
     dfs(0, n);
 }
-
 int main() {
     int n;
     cout << "Enter the number of numbers: ";
     cin >> n;
     solveNumbers(n);
-    for(vector<int> solution : solutions){
-        for(int i :solution){
-            cout<<i<<" ";
+    for (vector<int> solution: solutions) {
+        for (int i: solution) {
+            cout << i << " ";
         }
-        cout<<endl;
+        cout << endl;
     }
     return 0;
 }
